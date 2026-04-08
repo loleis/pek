e6dbd01e881aaca4480303edb55b967021aeddec
loleis <145881038+loleis@users.noreply.github.com>
2026-04-08 22:17:44 +0800

    Update 荐片.js

--- js/荐片.js
+++ js/荐片.js
@@ -1,159 +1,163 @@
-/**
- * 荐片 新式 JS0 接口源
- * 适配 FongMi TVBox 最新规范
+/*
 @header({
-  searchable: 1,
+  searchable: 2,
   filterable: 1,
-  quickSearch: 1,
-  title: '荐片',
-  lang: 'cat'
+  quickSearch: 0,
+  title: '荐片[优]',
+  '类型': '影视',
+  lang: 'ds'
 })
 */
 
-let host = 'https://api.ztcgi.com';
-let UA = 'Mozilla/5.0 (Linux; Android 9; V2196A Build/PQ3A.190705.08211809; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.114 Mobile Safari/537.36;webank/h5face;webank/1.0;netType:NETWORK_WIFI;appVersion:416;packageName:com.jp3.xg3';
-let imghost = '';
+var rule = {
+    title: '荐片[优]',
+    host: 'https://api.ztcgi.com',
+    homeUrl: '/api/dyTag/hand_data?category_id=88',
+    url: '/api/crumb/list?page=fypage&type=0&limit=24&fyfilter',
+    class_name: '电影&电视剧&动漫&短剧&综艺',
+    class_url: '1&2&3&67&4',
+    detailUrl: '/api/video/detailv2?id=fyid',
+    searchUrl: '/api/v2/search/videoV2?key=**&page=fypage',
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 1,
+    filter: {
+        "1": [{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"首推","v":"5"},{"n":"动作","v":"6"},{"n":"喜剧","v":"7"},{"n":"战争","v":"8"},{"n":"恐怖","v":"9"},{"n":"剧情","v":"10"},{"n":"爱情","v":"11"},{"n":"科幻","v":"12"},{"n":"动画","v":"13"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2025","v":"107"},{"n":"2024","v":"119"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+        "2": [{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"首推","v":"14"},{"n":"国产","v":"15"},{"n":"港台","v":"16"},{"n":"日韩","v":"17"},{"n":"海外","v":"18"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2025","v":"107"},{"n":"2024","v":"119"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+        "3": [{"key":"cateId","name":"分类","value":[{"n":"全部","v":"3"},{"n":"首推","v":"19"},{"n":"海外","v":"20"},{"n":"日本","v":"21"},{"n":"国产","v":"22"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2025","v":"107"},{"n":"2024","v":"119"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+        "4": [{"key":"cateId","name":"分类","value":[{"n":"全部","v":"4"},{"n":"首推","v":"23"},{"n":"国产","v":"24"},{"n":"海外","v":"25"},{"n":"港台","v":"26"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"极":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2025","v":"107"},{"n":"2024","v":"119"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"极":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","极":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+        "67": [{"key":"cateId","name":"分类","value":[{"n":"全部","v":"67"},{"n":"言情","v":"70"},{"n":"爱情","v":"71"},{"n":"战神","v":"72"},{"n":"古代","v":"73"},{"n":"萌娃","v":"74"},{"n":"神医","v":"75"},{"n":"玄幻","v":"76"},{"n":"重生","v":"77"},{"n":"激情","v":"79"},{"n":"时尚","v":"82"},{"n":"剧情演绎","v":"83"},{"n":"影视","v":"84"},{"n":"人文社科","v":"85"},{"n":"二次元","v":"86"},{"n":"明星八卦","v":"87"},{"n":"随拍","v":"88"},{"n":"个人管理","v":"89"},{"n":"音乐","v":"90"},{"n":"汽车","v":"91"},{"n":"休闲","v":"92"},{"n":"校园教育","极":"93"},{"n":"游戏","v":"94"},{"n":"科普","v":"95"},{"n":"科技","v":"96"},{"n":"时政社会","v":"97"},{"n":"萌宠","v":"98"},{"n":"体育","v":"99"},{"n":"穿越","v":"80"},{"n":"","v":"81"},{"n":"闪婚","v":"112"}]},{"key":"sort","name":"排序","value":[{"n":"全部","v":""},{"n":"最新","v":"update"},{"n":"最热","v":"hot"}]}]
+    },
 
-/**
- * 初始化配置
- */
-async function init(cfg) {
-    try {
-        let res = await req(`${host}/api/appAuthConfig`, { headers: { 'User-Agent': UA } });
-        let config = JSON.parse(res.content);
-        imghost = `https://${config.data.imgDomain}`;
-    } catch (e) {
-        imghost = 'https://img.jianpian.com';
-    }
-}
+    filter_url: 'area={{fl.area or "0"}}&sort={{fl.sort or "update"}}&year={{fl.year or "0"}}&category_id={{fl.cateId}}',
+    filter_def: {1:{cateId:'1'},2:{cateId:'2'},3:{cateId:'3'},4:{cateId:'4'},67:{cateId:'67'}},
+    headers: {'User-Agent': 'Mozilla/5.0 (Linux; Android 9; V2196A Build/PQ3A.190705.08211809; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.114 Mobile Safari/537.36;webank/h5face;webank/1.0;netType:NETWORK_WIFI;appVersion:416;packageName:com.jp3.xg3'},
+    timeout: 5000,
+    limit: 8,
+    play_parse: true,
 
-/**
- * 首页分类与筛选
- */
-async function home(filter) {
-    let classes = [
-        {type_id: '1', type_name: '电影'},
-        {type_id: '2', type_name: '电视剧'},
-        {type_id: '3', type_name: '动漫'},
-        {type_id: '4', type_name: '综艺'}
-    ];
-
-    // 筛选数据模板
-    const filterItem = [
-        {"key": "cateId", "name": "分类", "value": [{"v": "1", "n": "剧情"}, {"v": "2", "n": "爱情"}, {"v": "3", "n": "动画"}, {"v": "4", "n": "喜剧"}, {"v": "5", "n": "战争"}, {"v": "6", "n": "歌舞"}, {"v": "7", "n": "古装"}, {"v": "8", "n": "奇幻"}, {"v": "9", "n": "冒险"}, {"v": "10", "n": "动作"}, {"v": "11", "n": "科幻"}, {"v": "12", "n": "悬疑"}, {"v": "13", "n": "犯罪"}, {"v": "14", "n": "家庭"}, {"v": "15", "n": "传记"}, {"v": "16", "n": "运动"}, {"v": "18", "n": "惊悚"}, {"v": "20", "n": "短片"}, {"v": "21", "n": "历史"}, {"v": "22", "n": "音乐"}, {"v": "23", "n": "西部"}, {"v": "24", "n": "武侠"}, {"v": "25", "n": "恐怖"}]},
-        {"key": "area", "name": "地區", "value": [{"v": "1", "n": "国产"}, {"v": "3", "n": "中国香港"}, {"v": "6", "n": "中国台湾"}, {"v": "5", "n": "美国"}, {"v": "18", "n": "韩国"}, {"v": "2", "n": "日本"}]},
-        {"key": "year", "name": "年代", "value": [{"v": "107", "n": "2025"}, {"v": "119", "n": "2024"}, {"v": "153", "n": "2023"}, {"v": "101", "n": "2022"}, {"v": "118", "n": "2021"}, {"v": "16", "n": "2020"}, {"v": "7", "n": "2019"}, {"v": "22", "n": "2016"}, {"v": "2015", "n": "2015以前"}]},
-        {"key": "sort", "name": "排序", "value": [{"v": "update", "n": "最新"}, {"v": "hot", "n": "最热"}, {"v": "rating", "n": "评分"}]}
-    ];
-
-    let filterObj = {"1": filterItem, "2": filterItem, "3": filterItem, "4": filterItem};
-
-    return JSON.stringify({
-        class: classes,
-        filters: filterObj
-    });
-}
-
-/**
- * 首页推荐
- */
-async function homeVod() {
-    let html = await req(`${host}/api/slide/list?pos_id=88`, { headers: { 'User-Agent': UA, 'Referer': host } });
-    let res = JSON.parse(html.content);
-    let videos = res.data.map(item => ({
-        vod_id: item.jump_id,
-        vod_name: item.title,
-        vod_pic: item.thumbnail.includes('http') ? item.thumbnail : `${imghost}${item.thumbnail}`,
-        vod_remarks: ""
-    }));
-    return JSON.stringify({ list: videos });
-}
-
-/**
- * 分类列表
- */
-async function category(tid, pg, filter, extend) {
-    let url = `${host}/api/crumb/list?fcate_pid=${tid}&category_id=&area=${extend.area || ''}&year=${extend.year || ''}&type=${extend.cateId || ''}&sort=${extend.sort || ''}&page=${pg}`;
-    let html = await req(url, { headers: { 'User-Agent': UA, 'Referer': host } });
-    let res = JSON.parse(html.content);
-    let videos = res.data.map(item => ({
-        vod_id: item.id,
-        vod_name: item.title,
-        vod_pic: item.path.includes('http') ? item.path : `${imghost}${item.path}`,
-        vod_remarks: item.mask
-    }));
-    return JSON.stringify({
-        page: pg,
-        list: videos
-    });
-}
-
-/**
- * 详情页
- */
-async function detail(id) {
-    let html = await req(`${host}/api/video/detailv2?id=${id}`, { headers: { 'User-Agent': UA, 'Referer': host } });
-    let data = JSON.parse(html.content).data;
-    
-    // 线路处理：将“常规线路”显示为“边下边播”
-    let play_from = data.source_list_source.map(item => item.name).join('$$$').replace(/常规线路/g, '边下边播');
-    let play_url = data.source_list_source.map(play =>
-        play.source_list.map(({source_name, url}) => `${source_name}$${url}`).join('#')
-    ).join('$$$');
-
-    let vod = {
-        vod_id: data.id,
-        vod_name: data.title,
-        vod_year: data.year,
-        vod_area: data.area,
-        vod_remarks: data.mask,
-        vod_content: data.description,
-        vod_play_from: play_from,
-        vod_play_url: play_url,
-        vod_pic: data.thumbnail.includes('http') ? data.thumbnail : `${imghost}${data.thumbnail}`
-    };
-
-    return JSON.stringify({ list: [vod] });
-}
-
-/**
- * 搜索功能
- */
-async function search(wd, quick) {
-    let url = `${host}/api/v2/search/videoV2?key=${encodeURIComponent(wd)}&category_id=88&page=1&pageSize=20`;
-    let html = await req(url, { headers: { 'User-Agent': UA, 'Referer': host } });
-    let res = JSON.parse(html.content);
-    let videos = res.data.map(item => ({
-        vod_id: item.id,
-        vod_name: item.title,
-        vod_pic: item.thumbnail.includes('http') ? item.thumbnail : `${imghost}${item.thumbnail}`,
-        vod_remarks: item.mask
-    }));
-    return JSON.stringify({ list: videos });
-}
-
-/**
- * 播放解析
- */
-async function play(flag, id, flags) {
-    let playUrl = id;
-    // 判断是否需要添加专用协议前缀
-    if (!id.includes(".m3u8") && !id.includes(".mp4")) {
-        playUrl = `tvbox-xg:${id}`;
-    }
-    return JSON.stringify({
+    lazy: async (flag, id) => ({
         parse: 0,
-        url: playUrl
-    });
-}
+        url: id.includes(".m3u8") ? id : `tvbox-xg:${id}`
+    }),
+    parseList(html) {
+        const res = JSON.parse(html);
+        return res.data.map(item => ({
+            vod_id: item.jump_id || item.id,
+            vod_name: item.title,
+            vod_pic: `${this.imghost}${item.thumbnail}`,
+            vod_remarks: item.mask,
+            style: {"type": "rect", "ratio": 1.33}
+        }));
+    },
+    parseDetail(html) {
+        const res = JSON.parse(html).data;
+        const play_from = res.source_list_source.map(i => i.name === "常规线路" ? "边下边播" : i.name).join('$$$');
+        const play_url = res.source_list_source.map(play =>
+            play.source_list.map(({source_name, url}) => `${source_name}$${url}`).join('#')
+        ).join('$$$');
+        return {
+            type_name: '',
+            vod_year: res.year,
+            vod_area: res.area,
+            vod_remarks: res.mask,
+            vod_content: res.description,
+            vod_play_from: play_from,
+            vod_play_url: play_url
+        };
+    },
 
-// 导出标准接口对象
-export default {
-    init,
-    home,
-    homeVod,
-    category,
-    detail,
-    search,
-    play
+    预处理: async function() {
+        this.imghost = 'https://img1.vbwus.com';
+        try {
+            const res = JSON.parse(await request(this.host + '/api/appAuthConfig', {headers: this.headers}));
+            if (res?.data?.imgDomain) this.imghost = 'https://' + res.data.imgDomain;
+        } catch (e) {}
+    },
+
+    推荐: async function(tid, pg, filter, extend) {
+        await this.预处理();
+        const data = JSON.parse(await request(this.input)).data["20"];
+        return setResult(data.map(item => ({
+            title: item.title,
+            img: this.imghost + item.path,
+            desc: (item.mask || item.playlist?.title) + ' ⭐' + item.score,
+            url: item.id
+        })));
+    },
+
+    一级: async function(tid, pg, filter, extend) {
+        await this.预处理();
+        let apiUrl = this.input;
+        const realTid = String(tid);
+        if (realTid === '67') apiUrl = apiUrl.replace('/api/crumb/list', '/api/crumb/shortList');
+        if (realTid.endsWith('_clicklink')) {
+            const actualTid = realTid.split('_')[0];
+            apiUrl = `${this.host}/api/video/search?key=${actualTid}&page=${pg}`;
+        }
+        const data = JSON.parse(await request(apiUrl)).data;
+        return setResult(data.map(item => {
+            const isShort = realTid === '67';
+            const imgUrl = this.imghost + (isShort ? (item.cover_image || item.path) : (item.thumbnail || item.path));
+            return {
+                title: item.title,
+                img: imgUrl,
+                desc: (item.mask || item.playlist?.title) + ' ⭐' + item.score,
+                url: `${item.id}@${realTid}`
+            };
+        }));
+    },
+
+    二级: async function(ids) {
+        await this.预处理();
+        const [id, tid] = this.input.split('=')[1].split('@');
+        const isShort = tid === '67';
+        const path = isShort ? '/api/detail' : '/api/video/detailv2';
+        const param = isShort ? 'vid' : 'id';
+        const res = JSON.parse(await request(`${this.host}${path}?${param}=${id}`, {headers: this.headers})).data;
+        const vod = {
+            vod_id: id,
+            vod_name: res.title,
+            vod_pic: this.imghost + (isShort ? (res.cover_image || res.path) : (res.thumbnail || res.path)),
+            type_name: (res.types || []).map(t => t.name).join('/') || '',
+            vod_year: res.year || '',
+            vod_area: res.area || '',
+            vod_remarks: res.update_cycle || '',
+            vod_actor: (res.actors || []).map(a => a.name).join('/') || '',
+            vod_director: (res.directors || []).map(d => d.name).join('/') || '',
+            vod_content: res.description || ''
+        };
+        const rename = name => name === "常规线路" ? "边下边播线路" : name;
+        let sources = [];
+        if (isShort) {
+            const pl = res.playlist || [];
+            if (pl.length) {
+                const name = rename(pl[0]?.source_config_name || "短剧");
+                const urls = pl.map(ep => `${ep.title}$${ep.url}`).join('#');
+                sources.push({name, urls});
+            }
+        } else {
+            (res.source_list_source || []).forEach(src => {
+                const name = rename(src.name);
+                const urls = (src.source_list || []).map(ep => `${ep.source_name || ep.weight}$${ep.url}`).join('#');
+                if (urls) sources.push({name, urls});
+            });
+        }
+        if (sources.length) {
+            vod.vod_play_from = sources.map(s => s.name).join('$$$');
+            vod.vod_play_url = sources.map(s => s.urls).join('$$$');
+        }
+        return vod;
+    },
+
+    搜索: async function(wd, quick, pg) {
+        await this.预处理();
+        const data = JSON.parse(await request(this.input)).data;
+        return setResult(data.map(item => ({
+            title: item.title,
+            img: this.imghost + item.thumbnail,
+            desc: item.mask + ' ⭐' + item.score,
+            url: item.id
+        })));
+    }
 };
